3882d2f55503956d97ee57bd8c9ffc46b7c63ed2
Vydhi Vasudevan <vvasudevan@vydhi-v.emmes.com>
2018-03-27 17:15:16 -0400

    adding app.use

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 
 const app = express();
-
 const appPort = 5000;
+
+app.use((req, res, next) => {
+    req.name = 'TestNameParam';
+    next();
+});
+
 app.listen(appPort,()=>{
     console.log(`App started listening on port ${appPort}`);
 });
 
 //Index route
 app.get('/',(req, res) => {
+    console.log(`Logging name from requst -  ${req.name}`);
     res.send('Welcome to the app homepage!');
 })
 
@@ -16,3 +22,4 @@ app.get('/',(req, res) => {
 app.get('/about',(req, res) => {
     res.send('About app!');
 });
+
