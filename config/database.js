3329253a2fbe9f2b02d5bcbda99e856525f8487f
Vydhi Vasudevan <vvasudevan@vydhi-v.emmes.com>
2018-04-06 16:29:51 -0400

    ready to deploy finally??

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
-const mongoURIValue = 'mongodb://localhost/vidjot-dev'
 if(process.env.NODE_ENV === 'production'){
-    mongoURIValue = 'mongodb://ydswaran:mlabpwd1@ds237379.mlab.com:37379/vidjot-prod'
+    module.exports = {mongoURI:'mongodb://ydswaran:mlabpwd1@ds237379.mlab.com:37379/vidjot-prod'};
+}else{
+    module.exports = {mongoURI:'mongodb://localhost/vidjot-dev'};
 }
 
-module.exports = {mongoURI:mongoURIValue};
+ 
